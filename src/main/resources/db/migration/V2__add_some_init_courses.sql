19c848cbf1aada543caace063dc352cf37838c20
spencercjh <shouspencercjh@foxmail.com>
2021-04-15 15:33:28 +0800

    Add some init courses data

---
+++ src/main/resources/db/migration/V2__add_some_init_courses.sql
@@ -0,0 +1,40 @@
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (1, '2021-04-14 12:25:09', '2021-04-14 12:25:09', 1, '4ZS081001', '软件项目管理', '三[6-9](1-10周)', '嘉定校区1-331',
+        '202003', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (2, '2021-04-14 12:25:48', '2021-04-14 12:25:48', 2, '4ZS081002', '软件项目管理', '四[6-9](1-10周)', '嘉定校区1-331',
+        '202003', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (3, '2021-04-15 15:27:31', '2021-04-15 15:27:31', 3, '3ZS081002', '软件测试技术及应用', '一[6-9](1-10周)\\', '嘉定校区1-209
+', '202003', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (4, '2021-04-15 15:27:31', '2021-04-15 15:27:31', 4, '3ZS081004', '软件测试技术及应用', '二[6-9](1-10周)\\', '嘉定校区1-209
+', '202003', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (5, '2021-04-15 15:27:31', '2021-04-15 15:27:31', 1, '2ZS081003', '软件体系结构', '一[6-9](1-10周)', '嘉定校区1-233',
+        '202002', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (6, '2021-04-15 15:27:31', '2021-04-15 15:27:31', 2, '2ZS081004', '软件体系结构', '二[6-9](1-10周)', '嘉定校区1-233',
+        '202002', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (7, '2021-04-15 15:27:31', '2021-04-15 15:27:31', 5, '3ZS081003', '企业级应用开发技术', '五[7-10](1-10周)', '嘉定校区1-231',
+        '202001', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (8, '2021-04-15 15:29:38', '2021-04-15 15:29:38', 6, '3ZS081001', '软件建模方法', '三[6-9](1-10周)', '嘉定校区1-207',
+        '202002', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (9, '2021-04-15 15:29:38', '2021-04-15 15:29:38', 6, '2ZS081002', '软件形式方法基础', '五[1-4](1-10周)', '嘉定校区1-233',
+        '202002', '');
+INSERT INTO pourrfot.course (id, create_time, update_time, teacher_id, course_code, course_name, class_time,
+                             class_location, term, profile_photo)
+VALUES (10, '2021-04-15 15:32:24', '2021-04-15 15:32:24', 7, '3ZSL08401', '操作系统结构与分析', '二[7-10](1-10周)', '嘉定校区1-411',
+        '202002', '');
