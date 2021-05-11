2c525255eb915d8368ccdad94842a330ddc6d47a
spencercjh <shouspencercjh@foxmail.com>
2021-05-11 19:53:32 +0800

    Wrap CourseGroup CRUD

--- src/main/java/cn/edu/shu/pourrfot/server/controller/CourseGroupController.java
+++ src/main/java/cn/edu/shu/pourrfot/server/controller/CourseGroupController.java
@@ -1,10 +1,13 @@
 package cn.edu.shu.pourrfot.server.controller;
 
 import cn.edu.shu.pourrfot.server.model.CourseGroup;
+import cn.edu.shu.pourrfot.server.model.dto.Result;
 import cn.edu.shu.pourrfot.server.service.CourseGroupService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 import java.net.URI;
-import java.util.Optional;
 
 /**
  * @author spencercjh
@@ -33,10 +35,10 @@ public class CourseGroupController {
   private CourseGroupService courseGroupService;
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Page<CourseGroup>> page(@RequestParam(required = false, defaultValue = "1") Integer current,
-                                                @RequestParam(required = false, defaultValue = "10") Integer size,
-                                                @PathVariable @NotNull Integer courseId,
-                                                @RequestParam(required = false) String groupName) {
+  public ResponseEntity<Result<Page<CourseGroup>>> page(@RequestParam(required = false, defaultValue = "1") Integer current,
+                                                        @RequestParam(required = false, defaultValue = "10") Integer size,
+                                                        @PathVariable @NotNull Integer courseId,
+                                                        @RequestParam(required = false) String groupName) {
     QueryWrapper<CourseGroup> query = Wrappers.query(new CourseGroup());
     if (courseId != null) {
       query = query.eq(CourseGroup.COL_COURSE_ID, courseId);
@@ -44,35 +46,44 @@ public class CourseGroupController {
     if (StringUtils.isNotBlank(groupName)) {
       query = query.like(CourseGroup.COL_GROUP_NAME, groupName);
     }
-    return ResponseEntity.ok(courseGroupService.page(new Page<>(current, size), query));
+    return ResponseEntity.ok(Result.normalOk("Get course-groups page success",
+      courseGroupService.page(new Page<>(current, size), query)));
   }
 
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<CourseGroup> detail(@PathVariable @NotNull Integer courseId,
-                                            @PathVariable @NotNull Integer id) {
-    return ResponseEntity.of(Optional.ofNullable(courseGroupService.getById(id)));
+  @ApiResponses({@ApiResponse(code = 404, message = "Can't find course-group with the specific id", response = Result.class)})
+  public ResponseEntity<Result<CourseGroup>> detail(@PathVariable @NotNull Integer courseId,
+                                                    @PathVariable @NotNull Integer id) {
+    final CourseGroup found = courseGroupService.getById(id);
+    return found != null ? ResponseEntity.ok(Result.normalOk("Get course-group detail success", found)) :
+      ResponseEntity.status(HttpStatus.NOT_FOUND).body(Result.notFound("Not found course-group with the specific id"));
   }
 
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(code = HttpStatus.CREATED)
-  public ResponseEntity<CourseGroup> create(@NotNull @RequestBody @Validated CourseGroup courseGroup) {
+  public ResponseEntity<Result<CourseGroup>> create(@NotNull @RequestBody @Validated CourseGroup courseGroup) {
     courseGroupService.save(courseGroup);
     return ResponseEntity.created(
       URI.create(String.format("%s/courses/%d/groups/%d", contextPath, courseGroup.getCourseId(), courseGroup.getId())))
-      .body(courseGroup);
+      .body(Result.createdOk("Create course-group success", courseGroup));
   }
 
   @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<CourseGroup> update(@PathVariable @NotNull Integer courseId,
-                                            @PathVariable @NotNull Integer id,
-                                            @RequestBody @Validated @NotNull CourseGroup courseGroup) {
+  public ResponseEntity<Result<CourseGroup>> update(@PathVariable @NotNull Integer courseId,
+                                                    @PathVariable @NotNull Integer id,
+                                                    @RequestBody @Validated @NotNull CourseGroup courseGroup) {
     courseGroupService.updateById(courseGroup.setId(id).setCourseId(courseId));
-    return ResponseEntity.ok(courseGroup);
+    return ResponseEntity.ok(Result.normalOk("update course-group success", courseGroup));
   }
 
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(code = HttpStatus.NO_CONTENT)
+  @ApiResponses({@ApiResponse(code = 204, message = "Delete course-group success", response = Result.class),
+    @ApiResponse(code = 404, message = "Can't find the course-group with the specific id to delete", response = Result.class)})
   public ResponseEntity<?> delete(@PathVariable @NotNull Integer courseId, @PathVariable @NotNull Integer id) {
-    return courseGroupService.removeById(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    return courseGroupService.removeById(id) ? ResponseEntity.status(HttpStatus.NO_CONTENT)
+      .body(Result.deleteOk("Delete course-group success")) :
+      ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(Result.notFound("Can't find the course-group with the specific id to delete"));
   }
 }
