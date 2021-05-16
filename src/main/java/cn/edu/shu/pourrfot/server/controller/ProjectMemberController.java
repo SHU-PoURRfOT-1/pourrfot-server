fd82af79071ab1bb04fdb2207364e4577751e8df
spencercjh <shouspencercjh@foxmail.com>
2021-05-17 02:16:50 +0800

    Integrate project-member with user context;

--- src/main/java/cn/edu/shu/pourrfot/server/controller/ProjectMemberController.java
+++ src/main/java/cn/edu/shu/pourrfot/server/controller/ProjectMemberController.java
@@ -1,15 +1,14 @@
 package cn.edu.shu.pourrfot.server.controller;
 
 import cn.edu.shu.pourrfot.server.model.ProjectMember;
+import cn.edu.shu.pourrfot.server.model.dto.ProjectMemberUser;
 import cn.edu.shu.pourrfot.server.model.dto.Result;
 import cn.edu.shu.pourrfot.server.service.ProjectMemberService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -28,21 +27,21 @@ import java.net.URI;
 @RequestMapping("/projects/{projectId}/members")
 @Slf4j
 @Validated
+@Api(value = "Project-Members CRUD", authorizations = {@Authorization("admin"), @Authorization("teacher")}, tags = "Project-Members")
 public class ProjectMemberController {
   @Value("${server.servlet.contextPath}")
   private String contextPath;
   @Autowired
   private ProjectMemberService projectMemberService;
 
+  @ApiOperation(value = "project-members page",
+    notes = "admin users can access all projects' members;\n" +
+      "teacher and student users can only access their own projects' members.")
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Result<Page<ProjectMember>>> page(@RequestParam(required = false, defaultValue = "1") Integer current,
-                                                          @RequestParam(required = false, defaultValue = "10") Integer size,
-                                                          @PathVariable @NotNull Integer projectId,
-                                                          @RequestParam(required = false) String roleName) {
-    QueryWrapper<ProjectMember> query = Wrappers.query(new ProjectMember());
-    if (StringUtils.isNotBlank(roleName)) {
-      query = query.eq(ProjectMember.COL_ROLE_NAME, roleName);
-    }
+  public ResponseEntity<Result<Page<ProjectMemberUser>>> page(@RequestParam(required = false, defaultValue = "1") Integer current,
+                                                              @RequestParam(required = false, defaultValue = "10") Integer size,
+                                                              @PathVariable @NotNull Integer projectId) {
+    QueryWrapper<ProjectMemberUser> query = Wrappers.query(new ProjectMemberUser().setProjectId(projectId));
     if (projectId != null) {
       query = query.eq(ProjectMember.COL_PROJECT_ID, projectId);
     }
@@ -50,15 +49,21 @@ public class ProjectMemberController {
       projectMemberService.page(new Page<>(current, size), query)));
   }
 
+  @ApiOperation(value = "project-member detail",
+    notes = "admin users can access all projects' members;\n" +
+      "teacher and student users can only access their own projects' members.")
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiResponses({@ApiResponse(code = 404, message = "Can't find project-member with the specific id", response = Result.class)})
-  public ResponseEntity<Result<ProjectMember>> detail(@PathVariable @NotNull Integer projectId,
-                                                      @PathVariable @NotNull Integer id) {
-    final ProjectMember found = projectMemberService.getById(id);
+  public ResponseEntity<Result<ProjectMemberUser>> detail(@PathVariable @NotNull Integer projectId,
+                                                          @PathVariable @NotNull Integer id) {
+    final ProjectMemberUser found = projectMemberService.getProjectMemberUserById(id);
     return found != null ? ResponseEntity.ok(Result.normalOk("Get project-member detail success", found)) :
       ResponseEntity.status(HttpStatus.NOT_FOUND).body(Result.notFound("Can't found project-member with the specific id"));
   }
 
+  @ApiOperation(value = "create project-member",
+    notes = "admin users is unrestricted but can't create a project-member with student owner;\n" +
+      "teacher users can only create own projects' members.")
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(code = HttpStatus.CREATED)
   public ResponseEntity<Result<ProjectMember>> create(@PathVariable @NotNull Integer projectId,
@@ -69,6 +74,9 @@ public class ProjectMemberController {
       .body(Result.createdOk("Create project-member success, please pay attention to the LOCATION in headers", projectMember));
   }
 
+  @ApiOperation(value = "update project-member",
+    notes = "admin users is unrestricted;\n" +
+      "teacher users can only update own projects' members.")
   @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Result<ProjectMember>> update(@PathVariable @NotNull Integer projectId,
                                                       @PathVariable @NotNull Integer id,
@@ -77,6 +85,9 @@ public class ProjectMemberController {
     return ResponseEntity.ok(Result.normalOk("Update project-member success", projectMember));
   }
 
+  @ApiOperation(value = "delete project-member",
+    notes = "admin users is unrestricted;\n" +
+      "teacher users can only delete own projects' members.")
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(code = HttpStatus.NO_CONTENT)
   @ApiResponses({@ApiResponse(code = 204, message = "Delete project-member success", response = Result.class),
--- src/main/java/cn/edu/shu/pourrfot/server/service/ProjectMemberService.java
+++ src/main/java/cn/edu/shu/pourrfot/server/service/ProjectMemberService.java
@@ -1,6 +1,9 @@
 package cn.edu.shu.pourrfot.server.service;
 
 import cn.edu.shu.pourrfot.server.model.ProjectMember;
+import cn.edu.shu.pourrfot.server.model.dto.ProjectMemberUser;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -8,5 +11,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ProjectMemberService extends IService<ProjectMember> {
 
+  Page<ProjectMemberUser> page(Page<ProjectMemberUser> page, Wrapper<ProjectMemberUser> queryWrapper);
 
+  ProjectMemberUser getProjectMemberUserById(int projectMemberId);
 }
