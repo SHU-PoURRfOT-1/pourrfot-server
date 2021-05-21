6c0a2ec0f321ed13c84355c4203b5abdd8e05a25
spencercjh <shouspencercjh@foxmail.com>
2021-05-21 09:05:57 +0800

    Integrate OssFile with user context;

--- src/main/java/cn/edu/shu/pourrfot/server/service/OssFileService.java
+++ src/main/java/cn/edu/shu/pourrfot/server/service/OssFileService.java
@@ -12,5 +12,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface OssFileService extends IService<OssFile> {
   Page<CompleteOssFile> page(Page<OssFile> page, Wrapper<OssFile> queryWrapper);
 
+  boolean isAccessibleForCurrentUser(OssFile ossFile);
+
   CompleteOssFile getCompleteOssFileById(int id);
 }
